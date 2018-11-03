d1ecf73565ec306caa8afe726b9d3a6ee93b2e57
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-03 13:51:18 +0000

    added addPlaylist to the list of functions in the music library

--- library_prototypes.js
+++ library_prototypes.js
@@ -1,26 +1,4 @@
-// var library = {
-//   tracks: { t01: { id: "t01",
-//                    name: "Code Monkey",
-//                    artist: "Jonathan Coulton",
-//                    album: "Thing a Week Three" },
-//             t02: { id: "t02",
-//                    name: "Model View Controller",
-//                    artist: "James Dempsey",
-//                    album: "WWDC 2003"},
-//             t03: { id: "t03",
-//                    name: "Four Thirty-Three",
-//                    artist: "John Cage",
-//                    album: "Woodstock 1952"}
-//           },
-//   playlists: { p01: { id: "p01",
-//                       name: "Coding Music",
-//                       tracks: ["t01", "t02"]
-//                     },
-//                p02: { id: "p02",
-//                       name: "Other Playlist",
-//                       tracks: ["t03"]
-//                     }
-//              },
+// A music library implemented with JS prototypes
 
 let Library = function(name, creator) {
 
@@ -29,30 +7,39 @@ let Library = function(name, creator) {
   this.playlists = [];
 };
 
+
 let Playlist = function(name) {
 
   this.name = name;
   this.tracks = [];
 };
 
+
 Object.defineProperty(Playlist.prototype,
                       "overallRating",
                       {get: function() {
+
                         let rating = 0;
-                        this.tracks.forEach(track => {
+
+                        this.tracks.forEach((track) => {
                           rating += track.rating;
-                        })
-                        return rating / tracks.length;
+                        });
+
+                        return Math.round((rating / this.tracks.length) * 10) / 10;
                       }});
+
 
 Object.defineProperty(Playlist.prototype,
                       "totalDuration",
                       {get: function() {
+
                         let duration = 0;
+
                         this.tracks.forEach(track => {
-                          duration += track.duration;
+                          duration += track.length;
                         })
-                        return duration;
+
+                        return Math.round(duration / 60 * 10) / 10 + ' minutes';
                       }});
 
 
@@ -63,11 +50,19 @@ let Track = function(title, rating, length) {
   this.length = length;
 };
 
+
 Playlist.prototype.addTrack = function (track) {
 
-    this.tracks.unshift(track);
-    console.log('this.tracks: ', this.tracks);
+  this.tracks.unshift(track);
 };
+
+
+Library.prototype.addPlaylist = function(playlist) {
+
+  this.playlists.unshift(playlist);
+}
+
+
 
 let track1 = new Track('I want to hold your hand', 3, 144);
 let track2 = new Track('Shine on you crazy diamond', 5, 1560);
@@ -75,11 +70,18 @@ let track3 = new Track('Everything Now', 5, 303);
 
 let myPlaylist = new Playlist('Erika');
 myPlaylist.addTrack(track1);
-myPlaylist.addTrack(track1);
-myPlaylist.addTrack(track1);
+myPlaylist.addTrack(track2);
+myPlaylist.addTrack(track3);
+
+let myLibrary = new Library('Favourites', 'Erika');
+myLibrary.addPlaylist(myPlaylist);
+
 
 console.log('track1: ', track1);
 console.log('track2: ', track2);
 console.log('track3: ', track3);
 
 console.log("myPlaylist: ", myPlaylist);
+console.log("myPlaylist rating: ", `${myPlaylist.overallRating}`);
+console.log("myPlaylist duration: ", `${myPlaylist.totalDuration}`);
+console.log("myLibrary: ", myLibrary);
